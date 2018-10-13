6fa1a35c83f646c06276ecbb80115a4375dcb04e
Graham Dennis <graham.dennis@gmail.com>
2018-10-13 13:12:12 +1100

    /tags page.

---
+++ src/pages/tags.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import kebabCase from 'lodash/kebabCase'
+import Helmet from 'react-helmet'
+
+import Bio from '../components/Bio'
+import Layout from '../components/layout'
+import { rhythm } from '../utils/typography'
+
+class TagsPage extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const siteDescription = get(
+      this,
+      'props.data.site.siteMetadata.description'
+    )
+    const tags = this.props.data.allMarkdownRemark.group;
+
+    return (
+      <Layout location={this.props.location}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={siteTitle}
+        />
+        <Bio />
+        <h1>Tags</h1>
+        <ul>
+          {tags.map(tag => (
+            <li key={tag.fieldValue}>
+              <Link to={`/tags/${kebabCase(tag.fieldValue)}`}>
+                {tag.fieldValue} ({tag.totalCount})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    )
+  }
+}
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
