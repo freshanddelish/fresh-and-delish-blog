68e5f3b18520731c92c7c17268490086cf4b3845
Graham Dennis <graham.dennis@gmail.com>
2018-11-04 12:35:09 +1100

    Don't log

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -5,7 +5,6 @@ import kebabCase from 'lodash/kebabCase'
 const TagList = ({tags}) => (
     <div className="ph3 center measure-wide">
         <h1 className="f6 fw6 ttu tracked">Tags</h1>
-        {console.log('tags', {tags})}
         {tags.map(tag => 
             <Link key={tag} to={`/tags/${kebabCase(tag)}`} className="f6 link dim br-pill ph3 pv2 mb2 mh1 dib white bg-black">
                 {tag}
